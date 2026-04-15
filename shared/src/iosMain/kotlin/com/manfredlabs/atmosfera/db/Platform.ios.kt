e87cb145d9d11272e95a2e5a7d255209c184fa72
manfredlabs <caiomanfredini.10@hotmail.com>
2026-04-15 12:33:05 -0300

    fix: add missing import for timeIntervalSince1970 in Platform.ios.kt

--- shared/src/iosMain/kotlin/com/manfredlabs/atmosfera/db/Platform.ios.kt
+++ shared/src/iosMain/kotlin/com/manfredlabs/atmosfera/db/Platform.ios.kt
@@ -3,6 +3,7 @@ package com.manfredlabs.atmosfera.db
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
 import platform.Foundation.NSDate
+import platform.Foundation.timeIntervalSince1970
 
 actual fun currentTimeMs(): Long = (NSDate().timeIntervalSince1970 * 1000).toLong()
 
